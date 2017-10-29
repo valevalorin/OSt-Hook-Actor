54bd019df2bd9b85b123e3befa569bc8ccf6a8d8
valevalorin <leucochroic@gmail.com>
2017-10-29 15:10:11 -0500

    changed named of parameter being passed to sound agent

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 console.log(process.argv[2]);
 
-axios.post('http://localhost:10733/process-changed', { name: process.argv[2]} ) 
+axios.post('http://localhost:10733/process-changed', { processName: process.argv[2]} ) 
 	.then(function (response) {
 		console.log(response.data);
 	})
