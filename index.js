117edc2c3819f73cc9d46c8755fa8ddeba4368b1
valevalorin <leucochroic@gmail.com>
2017-11-04 16:52:44 -0500

    exit is now explicitly called after getting response from sound-agent

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log(process.argv[2]);
 axios.post('http://localhost:10733/process-changed', { processName: process.argv[2]} ) 
 	.then(function (response) {
 		console.log(response.data);
+		process.exit();
 	})
 	.catch(function (error) {
 		console.log(error);
